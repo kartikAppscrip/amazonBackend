9919e5fed3b6316d25d0f41a43504d7242d5dc9d
kartikAppscrip <114727287+kartikAppscrip@users.noreply.github.com>
2023-03-17 23:15:23 +0530

    Backend Repo Start
    Backend Repo Start

---
+++ index.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome');
+// });
+
+// app.listen(5000);
+const logger = require('./logger');
+console.log(logger.func(1), 'listening on port', module);
---
+++ logger.js
@@ -0,0 +1,7 @@
+const url = 'https://www.google.com/';
+const func = (value) => {
+    console.log('hello world', value);
+}
+
+module.exports.url = url;
+module.exports.func = func;
